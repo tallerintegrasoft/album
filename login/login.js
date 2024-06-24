1404a93fed1ef7aa2426844eea9f2df102a744d8
tallerintegrasoft <tallerintegrasoft@gmail.com>
2024-06-23 23:14:33 -0400

    Add files via upload

---
+++ login/login.js
@@ -0,0 +1,17 @@
+
+window.onload = () => 
+{ 
+    menuLateral({ querySelector: '#menu-lateral' });  
+}; 
+
+function entrar() 
+{
+    let username = document.querySelector("#username").value;
+    let password = document.querySelector("#password").value;
+
+    if (username = "" || password != "") {
+        localStorage.setItem("autorizado_album", "aaaaaaaaa");
+    }
+}
+
+
