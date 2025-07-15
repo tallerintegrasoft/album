5f4c7c5d4a33db94bd61da96095b058a2ec76e99
tallerintegrasoft <tallerintegrasoft@gmail.com>
2025-07-15 18:36:26 -0400

    Update catacumba.js

--- catacumba/catacumba.js
+++ catacumba/catacumba.js
@@ -97,7 +97,8 @@ function llenarDatos()
 		fotos: [
 		  { ruta: "1.jpg" }, { ruta: "2.jpg" }, { ruta: "3.jpg" }, { ruta: "4.jpg" }, { ruta: "5.jpg" }, { ruta: "6.jpg" }, { ruta: "7.jpg" }, { ruta: "8.jpg" },
 		  { ruta: "9.jpg" }, { ruta: "10.jpg" }, { ruta: "11.jpg" }, { ruta: "12.jpg" }, { ruta: "13.jpg" }, { ruta: "14.jpg" }, { ruta: "15.jpg" }, { ruta: "16.jpg" },
-		  { ruta: "17.jpg" }, { ruta: "18.jpg" }, { ruta: "19.jpg" }, { ruta: "20.jpg" }, { ruta: "21.jpg" }
+		  { ruta: "17.jpg" }, { ruta: "18.jpg" }, { ruta: "19.jpg" }, { ruta: "20.jpg" }, { ruta: "21.jpg" },
+		  { ruta: "22.jpg" }, { ruta: "23.jpg" }, { ruta: "24.jpg" }, { ruta: "25.jpg" }, { ruta: "26.jpg" }
 		]
 	});
 
